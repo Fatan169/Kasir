e98671d9f2c7df26ae3c1c9dd84084f186713347
Fatan Virgian Nova <53498573+Fatan169@users.noreply.github.com>
2019-11-12 19:48:39 +0700

    Add files via upload

---
+++ Anake.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jayabaru;
+import java.util.Scanner;
+/**
+ *
+ * @author asus
+ */
+public class Anake{
+   public static void main(String[] args) {
+        Mamake input = new Mamake();
+        input.ChellengeKasir();
+    }
+}
